eae33809bc539285da42254970c1aebfff8575e9
Netally-Lopes <aneliseenetally@gmail.com>
2026-09-15 14:18:58 -0300

    inicio

---
+++ src/test/java/api_tarefas/ds/DsApplicationTests.java
@@ -0,0 +1,13 @@
+package api_tarefas.ds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
