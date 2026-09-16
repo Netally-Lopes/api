3da7f8004f44d2516a45bc70a39c8622efd779e3
Netally-Lopes <aneliseenetally@gmail.com>
2026-09-16 15:52:50 -0300

    criando o task service

---
+++ src/main/java/api_tarefas/ds/repositories/TaskRepository.java
@@ -0,0 +1,26 @@
+package api_tarefas.ds.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import api_tarefas.ds.models.Task;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    List<Task> findByUser_Id(Long id);
+
+    /*
+    @Query(value = "SELECT t FROM Task t WHERE t.user.id = :id")
+    List<Task> findByUser_IdJPQL(@Param("id") Long id);
+    */
+
+    /*
+    @Query(value = "SELECT * FROM task t WHERE t.user_id = :id", nativeQuery = true)
+    List<Task> findByUser_IdSQL(@Param("id") Long id);
+    */
+}
